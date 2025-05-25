adb8bf4aa1cf420c2f1ef559014f4f07a685da00
Sedki2 <laatiri.sedki93@gmail.com>
2025-05-25 13:29:35 +0200

    Correction de la configuration Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,30 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Configuration Firebase (à remplacer par vos propres identifiants lors du déploiement)
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAAcArWv37e499EyreInkP9CmMzZCWNtHc",
-    authDomain: "pointage-app-noisy.firebaseapp.com",
-    projectId: "pointage-app-noisy",
-    storageBucket: "pointage-app-noisy.firebasestorage.app",
-    messagingSenderId: "187624415462",
-    appId: "1:187624415462:web:96848422f4bd63953e9b27",
-    measurementId: "G-SVF8EKENDQ"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Configuration Firebase (à remplacer par vos propres identifiants)
+const firebaseConfig = {
+  apiKey: "AIzaSyDummyKeyForDevelopment123456789",
+  authDomain: "pointage-app-noisy.firebaseapp.com",
+  projectId: "pointage-app-noisy",
+  storageBucket: "pointage-app-noisy.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890"
+};
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
